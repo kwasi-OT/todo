449ffb771ee92a1ca28610ca746b532061926918
Kwasi O-T <tweneboah21@gmail.com>
2023-09-22 00:38:18 +0000

    positioned and styked items container

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import TodoItem from './components/TodoItem';
 function App() {
   return (
     <div className="appContainer">
-      <div className='header'>
-        <div className='headerContent'>
-          <h1>TODO</h1>
-          <img src={moon} alt='moon icon'/>
-        </div>
-        <div className='formContainer'>
-          <Form/>
+      <div className='headerContainer'>
+        <div className='header'>
+          <div className='headerContent'>
+            <h1>TODO</h1>
+            <img src={moon} alt='moon icon'/>
+          </div>
+          <div className='formContainer'>
+            <Form/>
+          </div>
         </div>
       </div>
       <div className='itemsContainer'>
