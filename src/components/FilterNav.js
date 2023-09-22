1d95554c4f2d20982bf6a614190a388387843ffd
Kwasi O-T <tweneboah21@gmail.com>
2023-09-22 00:48:21 +0000

    created filter buttons

--- src/components/FilterNav.js
+++ src/components/FilterNav.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function FilterNav() {
     return (
         <div>
-        
+            <button type='button' className='filter'>All</button>
+            <button type='button' className='filter'>Active</button>
+            <button type='button' className='filter'>Completed</button>
         </div>
     )
 }
 
-export default FilterNav
+export default FilterNav;
